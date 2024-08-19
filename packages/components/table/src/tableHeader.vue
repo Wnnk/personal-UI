bbc2544fed22f12568ee3d90ae00fae9c79ca9c6
Wnnk <1275056222@.com>
2024-08-19 20:43:20 +0800

    <fix> 修复多列排序时显示同步问题

--- packages/components/table/src/tableHeader.vue
+++ packages/components/table/src/tableHeader.vue
@@ -29,17 +29,39 @@ onUnmounted(() => {
 })
 
 
-const sortType = ref<SortType>('');
+const sortType = ref<SortType[]>([]);
+/** 
+ * @description: 初始化排序类型
+**/
+const initSortType = () => {
+  for (let i = 0; i < props.columns.length; i++) {
+   sortType.value.push(''); 
+  }
+}
+
+onMounted(() => {
+  initSortType();
+})
 /** 
  * @description: 切换排序类型
  * @param {SortType} type 排序类型
  * @param {any} prop 排序属性
+ * @param {number} index 索引
   */
-const changeSortType = (type: SortType, prop: any) => {
-  if (type === sortType.value) return;
-  sortType.value = type;
+const changeSortType = (type: SortType, prop: any, index: number) => {
+  for (let i = 0; i < sortType.value.length; i++) {
+   if (i === index) {
+    console.log(sortType.value[i], type, sortType.value[i] === type);
+    if (type === sortType.value[i]) return;
+    sortType.value[i] = type;
+   } else {
+    sortType.value[i] = '';
+   }
+  }
   emits('toggleSort',  type, prop );
 }
+
+
 </script>
 
 <template>
@@ -50,12 +72,12 @@ const changeSortType = (type: SortType, prop: any) => {
       </colgroup>
       <thead :class="[bem.e('thead')]">
         <tr :class="[bem.e('tr')]">
-          <th :class="[bem.e('table-cell'), bem.is('border', props.border)]" v-for="column in props.columns" :key="column.prop" >
+          <th :class="[bem.e('table-cell'), bem.is('border', props.border)]" v-for="(column,index) of props.columns" :key="column.prop" >
             <div :class="[bem.e('cell')]">
               {{ column.label }}
               <span class="caret-wrapper" v-if="column.sort">
-                <i :class="['asc', sortType === 'asc'? 'is-asc' : '']" @click="changeSortType('asc', column.prop)"></i>
-                <i :class="['desc', sortType === 'desc'? 'is-desc' : '']" @click="changeSortType('desc', column.prop)"></i>
+                <i :class="['asc', sortType[index] === 'asc' ? 'is-asc' : '']" @click="changeSortType('asc', column.prop, index)"></i>
+                <i :class="['desc', sortType[index] === 'desc'? 'is-desc' : '']" @click="changeSortType('desc', column.prop, index)"></i>
               </span>
             </div>
           </th>
